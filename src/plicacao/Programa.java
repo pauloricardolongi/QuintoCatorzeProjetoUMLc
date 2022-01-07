d5475b720abcb27e4693e8ba6ab467eada5b828b
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-07 02:14:26 -0300

    versão com expressão lambda declarada

--- src/plicacao/Programa.java
+++ src/plicacao/Programa.java
@@ -3,8 +3,8 @@ package plicacao;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
-import auxiliar.ProductPredicado;
 import entidades.Product;
 
 public class Programa {
@@ -18,9 +18,11 @@ public class Programa {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-
+//expressão lambda declarada
         //remover da lista quem tiver o preço 100 ou mais
-		list.removeIf(Product::nonStaticProdPred);
+		
+		Predicate<Product> pred = p -> p.getPreco() >= 100.0;
+		list.removeIf(pred);
 		for(Product p : list) {
 			System.out.println(p);
 		}
